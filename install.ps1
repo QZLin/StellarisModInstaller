60097e113a0b6fa58451f82e053504961364efff
QZLin <qzlin01@163.com>
2022-01-24 02:03:35 +0800

    support mod folder

--- install.ps1
+++ install.ps1
@@ -1,40 +1,46 @@
-# 
 param(
     [Parameter(Mandatory = $true)][string]$mod_zip,
     [string]$_7zip = "C:\Program Files\7-Zip\7z.exe",
-    [string]$game_path = $PSScriptRoot
+    [string]$game_mod_path = $PSScriptRoot,
+    [switch]$Folder
 )
- 
 
-# if ($args.Length -eq 0) {
-#     Write-Output "E: Require argument: ``zipfile path``"
-#     exit
-# }
-Write-Output "I: Install`"$mod_zip`""
-Remove-Item -r "_tmpmod" -ErrorAction SilentlyContinue
-try { Expand-Archive $mod_zip -DestinationPath "_tmpmod" }
-catch {
-    Write-Output "Powershell fail to unzip, try 7zip"
-    Remove-Item -r "_tmpmod" -ErrorAction SilentlyContinue
-    & $_7zip x -o_tmpmod $mod_zip
+if (-not $Folder) {
+    # Unzip Mod Zip
+    Write-Output "I: Install`"$mod_zip`""
+    Remove-Item -r $mod_folder -ErrorAction SilentlyContinue
+    try { Expand-Archive $mod_zip -DestinationPath $mod_folder }
+    catch {
+        Write-Output "Powershell fail to unzip, try 7zip"
+        Remove-Item -r $mod_folder -ErrorAction SilentlyContinue
+        & $_7zip x -o$mod_folder $mod_zip
+    }
+    $mod_folder = "_tmpmod"
+}
+else {
+    $mod_folder = $mod_zip
 }
 
-
-$descriptor = Get-Content "_tmpmod/descriptor.mod"
-$r = Select-String -Pattern 'name="(.*?)"' -InputObject $descriptor
+# Get Mod Name in descriptor.mod
+$descriptor = Get-Content (Join-Path $mod_folder "descriptor.mod")
+$r = Select-String -Pattern 'name\s*=\s*"(.*?)"' -InputObject $descriptor
 if ($r.Matches) {
     $modname = $r.Matches.Groups[1].Value
     $codename = $modname `
         -replace " ", "_" -replace "-", "_" -replace "'", "" -replace ":", "_"
     Write-Output "I: `"$modname`"->`"$codename`""
 
+    $descriptor = $descriptor -replace "\s*path\s*=\s*(?:\S|.)*", ""
     $descriptor += "path=`"mod/$codename`""
-    try { Move-Item "_tmpmod" $codename -ErrorAction Stop }
-    catch { Move-Item "_tmpmod" $codename -Force -Confirm }
+
+    if (! (Join-Path (Convert-Path $mod_folder) "" ) -eq (Join-Path (Convert-Path (Join-Path $game_mod_path $codename)) "")) {
+        try { Move-Item $mod_folder $codename -ErrorAction Stop }
+        catch { Move-Item $mod_folder $codename -Force -Confirm }
+    }
     
     $descriptor > "$codename.mod"
     Write-Output "I: Sucuss"
 }
 else {
-    Write-Output "E: descriptor.mod not found, exit..."
+    Write-Output "E: descriptor.mod not avaliable, exit..."
 }
